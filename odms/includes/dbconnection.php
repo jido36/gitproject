43e10b1bce76a7e8cc2dea300c9e56a975fb5b44
okitikan olugbenga <okitikanolugbenga@gmail.com>
2020-02-17 19:06:07 +0100

    added docummentation to sql commection

--- odms/includes/dbconnection.php
+++ odms/includes/dbconnection.php
@@ -5,6 +5,7 @@ define('DB_USER','root');
 define('DB_PASS','');
 define('DB_NAME','odmsdb');
 // Establish database connection.
+// a comment to fake file update
 try
 {
 $dbh = new PDO("mysql:host=".DB_HOST.";dbname=".DB_NAME,DB_USER, DB_PASS,array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES 'utf8'"));
