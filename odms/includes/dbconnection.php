9b539794c7ae4d799138f1bcf67b5e308b867be7
Jido <okitikanolugbenga@gmail.com>
2020-02-18 11:41:36 +0000

    jidos commit

--- odms/includes/dbconnection.php
+++ odms/includes/dbconnection.php
@@ -6,6 +6,7 @@ define('DB_PASS','');
 define('DB_NAME','odmsdb');
 // Establish database connection.
 // a comment to fake file update
+// jidos comment
 try
 {
 $dbh = new PDO("mysql:host=".DB_HOST.";dbname=".DB_NAME,DB_USER, DB_PASS,array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES 'utf8'"));
